5396b2b5cfaa00218e08b28dfdfd6c14f7b4461c
theMasix <smasihsajadi@gmail.com>
2021-02-01 08:14:35 +0330

    chore: add dayjs with jalaliday
    - also added a utility function for it

---
+++ src/utils/date.js
@@ -0,0 +1,12 @@
+// This file configs dayjs with jalaliday plugin to support jalali calendar
+import dayjs from 'dayjs';
+import jalaliday from 'jalaliday';
+
+dayjs.extend(jalaliday);
+dayjs.calendar('jalali');
+
+// Here you can add utility functions for dayjs
+
+const toLocalDate = (datetime, format = 'YYYY/M/D') => dayjs(datetime).format(format)
+
+export { toLocalDate };
