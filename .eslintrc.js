e5b627a479979c718ef2666609af2b297f97ea21
theMasix <smasihsajadi@gmail.com>
2021-02-01 12:53:14 +0330

    chore: add react-app config to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,42 +4,44 @@ module.exports = {
     node: true,
   },
   extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "prettier",
-    "prettier/react",
-    "plugin:jsx-a11y/recommended",
+    'react-app',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    'prettier/react',
+    'plugin:jsx-a11y/recommended',
   ],
-  plugins: ["jsx-a11y"],
+  plugins: ['jsx-a11y'],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
   },
   rules: {
-    "react/jsx-filename-extension": [
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".js", ".jsx"],
+        extensions: ['.js', '.jsx'],
       },
     ],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
+        js: 'never',
+        jsx: 'never',
       },
     ],
 
     // Warn
-    "react/jsx-props-no-spreading": "warn",
-    "no-unused-expressions": "warn",
-    "no-console": "warn",
+    'react/jsx-props-no-spreading': 'warn',
+    'no-unused-expressions': 'warn',
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
 
     // Off
-    "react/react-in-jsx-scope": "off",
-    "import/prefer-default-export": "off",
-    "no-shadow": "off",
-    "max-len": "off",
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off',
+    'max-len': 'off',
   },
 };
